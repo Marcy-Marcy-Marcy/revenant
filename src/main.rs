911ad1261beb7c5bde44cb5ef521c7b12502865e
Marcy <193825312+Marcy-Marcy-Marcy@users.noreply.github.com>
2025-01-13 20:28:35 +0000

    Create an App object instead of just running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod main_window;
+use fltk::{app, prelude::WidgetExt};
+use main_window::MainWindow;
+
 fn main() {
-    println!("Hello, world!");
+    let app = app::App::default();
+    let mut window = MainWindow::new(400, 400);
+    window.show();
+    app.run().expect("The application should be able to run")
 }
